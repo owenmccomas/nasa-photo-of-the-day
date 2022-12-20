5a12107c97693e59c589174e57a23143dad95d12
Owen McComas <owen@mccomas.me>
2022-12-19 19:36:02 -0700

    SImple app completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect, useRef, setImageTop }from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+  const [nasaData, setNasaData] = useState({})
+  const imageRef = useRef(null)
+  const setImageTop = () => {
+    document.documentElement.style.setProperty('--image-top', `${imageRef.current.offsetTop}px`);
+  }
+
+  useEffect(() => {
+    getPic();
+    setImageTop();
+  }, []);
+
+  const getPic = () => {    
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=jmxgN4PiRb1149yWEV1ShOQZiG56kvIHmmSOXlYa")
+      .then(res => setNasaData(res.data))
+      .catch(err => console.log(err));}
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className="container">
+      <h1>NASA Photo of the Day</h1>
+      <h3 className="title">-{nasaData.title}-</h3>
+      <a href={nasaData.hdurl}><img src={nasaData.url} alt='nasa photo of the day' ref={imageRef}/></a>
+      <h3 className="hdurl">Click image for an HD version</h3>
+      <div className='explanation'><p>{nasaData.explanation}</p></div>
     </div>
   );
 }
